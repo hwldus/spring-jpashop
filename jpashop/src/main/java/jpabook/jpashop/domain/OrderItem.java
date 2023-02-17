ad4b05780fd3ded5cdb083b2d8b0ba51ee22d6d9
JiYeon <89963270+hwldus@users.noreply.github.com>
2023-02-17 18:02:30 +0900

    상품, 주문 도메인 개발

--- jpashop/src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -23,4 +23,29 @@ public class OrderItem {
 
     private int orderPrice; // 주문 가격
     private int count; //주문 수량
+
+    //==생성 메서드==//
+    public static OrderItem createOrderItem(Item item, int orderPrice, int count) {
+        OrderItem orderItem = new OrderItem();
+        orderItem.setItem(item);
+        orderItem.setOrderPrice(orderPrice);
+        orderItem.setCount(count);
+
+        item.removeStock(count);
+        return orderItem;
+    }
+
+    //==비즈니스 로직==//
+    public void cancel() { //취소시 재고를 하나 올리기
+        getItem().addStock(count);
+    }
+
+    //==조회 로직==//
+
+    /**
+     * 주문상품 가격 전체 조회
+     */
+    public int getTotalPrice() {
+        return getOrderPrice() * getCount();
+    }
 }
